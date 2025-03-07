7e0a91ed1ed41ee175882d1296e95821d14ab882
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2025-03-07 20:45:47 +0100

    Improved menu styles fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Menu } from './components/menu'
 function App() {
 
   return (
-    <>
-      <Menu>
-        <Menu.Item value="home" onClick={() => console.log('Home clicked')}>
-          Home
-        </Menu.Item>
-        <Menu.ItemLink value="about" href="/about">
-          About
-        </Menu.ItemLink>
-        <Menu.ItemLink value="contact" href="/contact"> 
-          Contact
-        </Menu.ItemLink>
-      </Menu>
+    <>   
+      <div style={{display:'flex'}}>
+        Esto es una prueba
+        <Menu>
+          <Menu.Item value="home" onClick={() => console.log('Home clicked')}>
+            Home
+          </Menu.Item>
+          <Menu.ItemLink value="about" href="/about">
+            About
+          </Menu.ItemLink>
+          <Menu.ItemLink value="contact" href="/contact"> 
+            Contact
+          </Menu.ItemLink>
+        </Menu>
+      </div>
     </>
   )
 }
